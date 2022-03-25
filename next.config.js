e9fd318f48142aa775aa10fafc054fd67e88e154
loc <locloc408@gmail.com>
2022-03-25 15:06:53 +0700

    fix

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ module.exports = {
   env: {
     NEXT_PUBLIC_MONGODB_URI:
       "mongodb+srv://locloc408:loc1792001@cluster0.mursv.mongodb.net/FacebookClone?retryWrites=true&w=majority",
-    NEXT_PUBLIC_BASE_URI: "http://localhost:3000/api",
+    NEXT_PUBLIC_BASE_URI:
+      "https://facebook-mini-nextjs-tailwindcss.vercel.app/api",
   },
 };
